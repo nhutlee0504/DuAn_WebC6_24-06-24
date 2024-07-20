6f91289cc498a314c69b1a06fd78e2e546fa568b
nhutlee0504 <leminhnhut718@gmail.com>
2024-07-20 19:30:31 +0700

    Commit lần đầu đưa các file dự án lên git

---
+++ DA_C6/WebAssemly/WebAssemly/Program.cs
@@ -0,0 +1,29 @@
+using WebAssemly.Client.Pages;
+using WebAssemly.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(WebAssemly.Client._Imports).Assembly);
+
+app.Run();
