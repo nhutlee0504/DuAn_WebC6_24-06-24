d328d9084f8bc58b73d98a59cddd06251b52570b
minhnhtpc06050 <nguyenminh21112004@gmail.com>
2024-07-22 12:25:19 +0700

    xóa web Server

--- DA_C6/API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ DA_C6/API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db8bdae54d36f5922bbcab241459298cbb2a8ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a07891cce97761f3adfc7193670654a186a0985")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
