eda7b37542f4d6e9947cf519a4dd6a6a4941a505
minhdqpc06173 <minhdqpc06173@fpt.edu.vn>
2024-07-22 12:26:33 +0700

    xóa blazor cũ

--- DA_C6/API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ DA_C6/API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56201f65086f393925ac995b96e3843b273e7ec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17fb6dc010cef718f193b7dc37fc307be19ace59")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
