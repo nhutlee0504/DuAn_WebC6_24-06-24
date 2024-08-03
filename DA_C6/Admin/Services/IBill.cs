da437129546c1ad4159adb710c371e509c6b0283
hungphan0703 <phanhung.07032004@gmail.com>
2024-08-03 22:28:37 +0700

    commit

---
+++ DA_C6/Admin/Services/IBill.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Admin.Data;
+using Admin.Model;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Admin.Services
+{
+    public interface IBill
+    {
+        public IEnumerable<Bill> GetAllBill();
+
+        public Bill GetBillId(int id);
+    }
+}
