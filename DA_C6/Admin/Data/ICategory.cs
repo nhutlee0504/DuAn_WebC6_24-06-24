3553dde826963762aa3b4142c3aaf1739f731406
minhdqpc06173 <minhdqpc06173@fpt.edu.vn>
2024-08-04 22:38:34 +0700

    cm

---
+++ DA_C6/Admin/Data/ICategory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Admin.Data
+{
+    public interface ICategory
+    {
+        public IEnumerable<Category> GetCategories();
+        public Category GetCategoryById(int id);
+        public Category AddCategory(Category category);
+        public Category UpdateCategory(int id, Category category);
+    }
+}
---
+++ DA_C6/Blazor/Server/Services/ISessionServices.cs
@@ -0,0 +1,7 @@
+﻿namespace Blazor.Server.Services
+{
+    public interface ISessionServices
+    {
+        string GetUsername();
+    }
+}
