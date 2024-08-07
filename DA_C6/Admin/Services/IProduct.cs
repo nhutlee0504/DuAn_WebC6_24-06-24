9aca235a301b1cbcc8d08983055bc70415653a91
PhamHuy <163984306+PhamHuy092004@users.noreply.github.com>
2024-08-07 19:14:00 +0700

    Commit

--- DA_C6/Admin/Services/IProduct.cs
+++ DA_C6/Admin/Services/IProduct.cs
@@ -4,21 +4,17 @@ using Admin.Model;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Admin.Services
 {
     public interface IProduct
     {
-        public IEnumerable<Product> GetProducts();
-
-        public Product GetProductId(int id);
-
-        public Product Add(Product product);
-
-        public Product Update(Product product, int id);
-
-        public void Delete(int id);
-
-        IEnumerable<Product> GetProductsWithDetails(); // Bổ sung phương thức này
+        Task<IEnumerable<Product>> GetProductsAsync(); // Sửa phương thức này
+        Task<Product> GetProductByIdAsync(int id); // Sửa phương thức này
+        Task<Product> AddAsync(Product product); // Sửa phương thức này
+        Task<Product> UpdateAsync(Product product, int id); // Sửa phương thức này
+        Task DeleteAsync(int id); // Sửa phương thức này
+        Task<IEnumerable<Product>> GetProductsWithDetailsAsync(); 
     }
 }
--- DA_C6/Admin/Startup.cs
+++ DA_C6/Admin/Startup.cs
@@ -7,10 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Admin
 {
@@ -24,34 +20,30 @@ namespace Admin
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
-			//services.AddControllersWithViews();
-			//services.AddRazorPages();
 
-			services.AddDbContext<ApplicationDbContext>(options =>
-		    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // Register the DbContext with dependency injection
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            // Register application services
             services.AddScoped<Services.CategoryResponse>();
             services.AddScoped<Services.SizeResponse>();
             services.AddScoped<Services.ColorResponse>();
             services.AddScoped<Services.SupplierResponse>();
-
             services.AddScoped<Services.AccountResponse>();
             services.AddScoped<Services.ProductResponse>();
             services.AddScoped<Services.ProductDetailResponse>();
             services.AddScoped<IImage, ImageResponse>();
             services.AddScoped<IEvaluate, EvaluateResponse>();
-            services.AddScoped<IBill,BillResponse>();
+            services.AddScoped<IBill, BillResponse>();
             services.AddScoped<Services.BillResponse>();
             services.AddScoped<Services.BillDetailResponse>();
             services.AddScoped<ISale, SaleResponse>();
             services.AddScoped<ICart, CartResponse>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
