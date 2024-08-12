60f94cf286aa0c2e5d412291570c7f72236a5cb9
minhnhtpc06050 <nguyenminh21112004@gmail.com>
2024-08-12 10:57:42 +0700

    commit login gg

--- DA_C6/Blazor/Server/Controllers/AccountController.cs
+++ DA_C6/Blazor/Server/Controllers/AccountController.cs
@@ -11,6 +11,13 @@ using Blazor.Model;
 using Account = Blazor.Shared.Model.Account;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Blazor.Server.Data;
+using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace Blazor.Server.Controllers
 {
@@ -19,7 +26,12 @@ namespace Blazor.Server.Controllers
     public class AccountController : ControllerBase
     {
         private IAccount account;
-        public AccountController(IAccount acc) => account = acc;
+        private readonly ApplicationDbContext _context;
+        public AccountController(IAccount acc, ApplicationDbContext context)
+        {
+            _context = context;
+            account = acc;
+        }
 
         [HttpGet]
         [Route("GetAll")]
@@ -53,6 +65,53 @@ namespace Blazor.Server.Controllers
                 Gender = acc.Gender,
                 Name = acc.Name
             });
+        }
+        [HttpGet("signin-google")]
+        public IActionResult SignInGoogle()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("GoogleResponse")
+            };
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+        [HttpGet("google-response")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var authenticateResult = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            if (!authenticateResult.Succeeded)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+
+            // Lấy thông tin người dùng từ principal
+            var email = authenticateResult.Principal.FindFirst(ClaimTypes.Email)?.Value;
+
+            // Gọi API hoặc truy xuất dữ liệu từ cơ sở dữ liệu để lấy thông tin tài khoản người dùng
+            var user = _context.Accounts.FirstOrDefault(u => u.Email == email);
+
+            if (user == null)
+            {
+                // Xử lý khi không tìm thấy người dùng
+
+                var newUserName = "NguoiDung" + _context.Accounts.Count() + 1; // Tạo tên người dùng mới
+                var newUser = new Account()
+                {
+                    UserName = newUserName,
+                    Password = "25d55ad283aa400af464c76d713c07ad",  // Mật khẩu tạm thời, bạn nên tạo một mật khẩu an toàn
+                    Email = email,
+                    Role = "Member",
+                    Name = " ",
+                    Phone = " ",
+                    Gender = "Nam",
+                    Address = " "
+                };
+                _context.Accounts.Add(newUser);
+                _context.SaveChanges();
+                user = newUser;
+            }
+            return LocalRedirectPreserveMethod($"/response-google?userName={user.UserName}"); // Chuyển hướng sau khi đăng nhập thành công
         }
 
         [HttpGet("{user}")]
